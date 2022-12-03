23b3958877202a695d3ed3811db445374cfc3243
Saipravallika <81575366+gayathribalaji05@users.noreply.github.com>
2022-12-03 14:16:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var Doctor;
+var Vaccine;
+var Corona;
+
+
+function preload(){
+
+Doctor=loadImage("Doctor.png/assests");
+Vaccine=loadImage("Vaccine.png/assests");
+Corona=loadImage("Corona.png/assests");
+
+}
+
+
+function setup() 
+Doctor.createSprite(200,200,20,20);
+
+{
+
+}
+
+function draw() 
+{
+
+}
